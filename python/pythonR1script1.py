d13cbeb97d7b9e94a09f4d6445d8324e9380ded9
Eddie Valbuena <valbuena.tech@gmail.com>
2024-05-23 17:05:06 -0700

    Edit pythonR1scipt1.py

--- python/pythonR1script1.py
+++ python/pythonR1script1.py
@@ -1,28 +1,28 @@
 import getpass
-import sys
 import telnetlib
 
 HOST = "192.168.122.227"
-user = raw_input("Enter your telnet username: ")
+user = input("Enter your telnet username: ")
 password = getpass.getpass()
 
 tn = telnetlib.Telnet(HOST)
 
-tn.read_until("Username")
-tn.write(user + "\n")
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
 if password:
-    tn.read_until("Password: ")
-    tn.write(password + "\n")
-    tn.write("enable\n")
-    tn.write("cisco\n")
-    tn.write("conf t\n")
-    tn.write("int loop 0\n")
-    tn.write("ip address 1.1.1.1 255.255.255.255\n")
-    tn.write("int loop 1\n")
-    tn.write("ip address 2.2.2.2 255.255.255.255\n")
-    tn.write("router ospf 1\n")
-    tn.write("network 0.0.0.0 255.255.255.255 area 0\n")
-    tn.write("end\n")
-    tn.write("exit\n")
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
 
-    print tn.read_all()
+    tn.write(b"enable\n")
+    tn.write(b"cisco\n")
+    tn.write(b"conf t\n")
+    tn.write(b"int loop 0\n")
+    tn.write(b"ip address 1.1.1.1 255.255.255.255\n")
+    tn.write(b"int loop 1\n")
+    tn.write(b"ip address 2.2.2.2 255.255.255.255\n")
+    tn.write(b"router ospf 1\n")
+    tn.write(b"network 0.0.0.0 255.255.255.255 area 0\n")
+    tn.write(b"end\n")
+    tn.write(b"exit\n")
+
+print(tn.read_all().decode('ascii'))
